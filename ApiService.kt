7e53a6c753f51e64a7becb36f818c3a5a591be4a
Natalia Stephania Rodriguez Araque <165220423+StephaniaA12@users.noreply.github.com>
2025-10-11 19:45:01 -0500

    Update ApiService.kt

--- ApiService.kt
+++ ApiService.kt
@@ -5,7 +5,7 @@ import retrofit2.http.GET
 import retrofit2.http.Url
 
 interface ApiService {
-    // usamos @Url para pasar la URL completa (gviz json)
     @GET
     suspend fun obtenerSheetRaw(@Url url: String): Response<String>
 }
+
